2b7fb3202918850340394db3f7f4e6e9dbe1a21f
Pavan <pavandantu18@gmail.com>
2024-02-06 23:15:00 +0530

    Type safety

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import products from '@/assets/data/products';
+import products from '@assets/data/products';
 import { StyleSheet,View,Text,Image } from 'react-native';
-import ProductListItem from '@/src/components/ProductListItem';
+import ProductListItem from '@/components/ProductListItem';
 
 export default function MenuScreen() {
   return (
